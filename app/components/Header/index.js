bd12e0db03e8ddc450b62659d215de224371d5d4
siva pavan <siva.pavan@juspay.in>
2018-04-14 22:28:09 +0530

    Add App logo

---
+++ app/components/Header/index.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div className='container'>
+      <div className='logo'>
+        Phenopod<sup> β</sup>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
