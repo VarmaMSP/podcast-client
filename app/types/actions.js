e128e8c5f6d13d026422bb4402c085d7ac56dafe
siva pavan <siva.pavan@juspay.in>
2018-04-20 13:27:37 +0530

    Add action types for corresponding store fields.

--- app/types/actions.js
+++ app/types/actions.js
@@ -1,6 +1,9 @@
 // @flow
 import type {Podcast, Episode} from './podcast';
 
+export type PodcastAction = SelectEpisodeAction
+export type NowPlayingAction = SelectEpisodeAction
+
 export type SelectPodcastAction = {|
   type: string,
   podcast: Podcast
