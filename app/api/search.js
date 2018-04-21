3168e3b3dd19ee24bbadb4ee923bd74326c677a2
siva pavan <siva.pavan@juspay.in>
2018-04-21 12:31:32 +0530

    Add function to search itunes based on a keyword.

---
+++ app/api/search.js
@@ -0,0 +1,27 @@
+// @flow
+import type {Podcast} from '../types/podcast';
+
+import request from 'request';
+
+export default function fetchPodcasts(term: string, limit: number): Promise<Array<Podcast>> {
+  let itunesAPI = encodeURI(`https://itunes.apple.com/search?entity=podcast&&limit=${limit}&&term=${term}`);
+  return new Promise((resolve, reject) => {
+    request(itunesAPI, (err, response, body) => {
+      if (err) return reject();
+      try {
+        let { results } = JSON.parse(body);
+        let podcasts: Array<Podcast> = results.map(p => ({
+          title    : p.collectionName,
+          artist   : p.artistName,
+          imageUrl : p.artworkUrl100.replace(/\/[^\/]*$/, ''),
+          feedUrl  : p.feedUrl
+        }));
+        podcasts.length > 0
+          ? resolve(podcasts)
+          : reject();
+      } catch(err) {
+        reject();
+      }
+    });
+  });
+}
