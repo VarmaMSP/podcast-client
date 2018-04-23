d87528e1940bc99ea0a09e9336dd67173787a3f4
siva pavan <siva.pavan@juspay.in>
2018-04-23 17:44:14 +0530

    Remove inital redux state.

--- app/index.js
+++ app/index.js
@@ -12,31 +12,7 @@ import Footer from './components/Footer';
 import Podcast from './components/Podcast';
 import SearchResults from './components/SearchResults';
 
-const initState: State = {
-  podcast: {
-    title: "Waking Up with Sam Harris",
-    artist: "Sam Harris",
-    imageUrl: "https://is3-ssl.mzstatic.com/image/thumb/Music128/v4/4f/df/51/4fdf5122-5b7d-1fa2-b04e-9ff6317aff27/source",
-    feedUrl: "http://wakingup.libsyn.com/rss"
-  },
-  nowPlaying: {
-    podcast: {
-      title: "Waking Up with Sam Harris",
-      artist: "Sam Harris",
-      imageUrl: "https://is3-ssl.mzstatic.com/image/thumb/Music128/v4/4f/df/51/4fdf5122-5b7d-1fa2-b04e-9ff6317aff27/source",
-      feedUrl: "http://wakingup.libsyn.com/rss"
-    },
-    episode: {
-      title: "#123  Identity & Honesty",
-      date: "9 April, 2018",
-      description: '',
-      link: "http://traffic.libsyn.com/wakingup/Waking_Up_123_Klein.mp3?dest-id=480596",
-      fileType: "audio/mpeg"
-    }
-  }
-}
-
-const store: Store = createStore(reducers, initState);
+const store: Store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
