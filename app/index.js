e470b0e7b14ad7db015ced37d02088171f7140a3
siva pavan <siva.pavan@juspay.in>
2018-04-23 11:01:00 +0530

    Use React router to route across the app.

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 import reducers from './reducers';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Podcast from './components/Podcast';
+import SearchResults from './components/SearchResults';
 
 const initState: State = {
   podcast: {
@@ -38,17 +40,19 @@ const store: Store = createStore(reducers, initState);
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      {/* HEADER */}
-      <Header/>
-      {/* MAIN CONTENT*/}
-      <div className='main-content container'>
-        <Podcast/>
+    <Router>
+      <div>
+        <Header/>
+        <div className='main-content container'>
+          <Switch>
+            <Route exact path='/podcast' component={Podcast}/>
+            <Route exact path='/search/:term' component={SearchResults}/>
+          </Switch>
+        </div>
+        <input type='checkbox' style={{display: 'none'}} id='footer-state'/>
+        <Footer/>
       </div>
-      {/* FOOTER */}
-      <input type='checkbox' style={{display: 'none'}} id='footer-state'/>
-      <Footer/>
-    </div>
+    </Router>
   </Provider>,
   (document.getElementById('app'): any)
 );
