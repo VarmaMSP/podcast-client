1bc1546b0d64d6086a4afd28188805881220a9f4
siva pavan <siva.pavan@juspay.in>
2018-04-30 01:06:53 +0530

    Store user feed to local storage.

--- app/utils/localStorage.js
+++ app/utils/localStorage.js
@@ -3,24 +3,29 @@ import type {State} from '../types/index';
 
 export const loadState = (): State => {
   try {
+    let userFeed = localStorage.getItem('user-feed');
     let subscriptions = localStorage.getItem('subscriptions');
     return {
       nowPlaying: undefined,
       podcast: undefined,
-      subscriptions: subscriptions ? JSON.parse(subscriptions) : []
+      subscriptions: subscriptions ? JSON.parse(subscriptions) : [],
+      userFeed: userFeed ? JSON.parse(userFeed) : []
     };
   } catch (err) {
     return {
       nowPlaying: undefined,
       podcast: undefined,
-      subscriptions: []
+      subscriptions: [],
+      userFeed: []
     };
   }
 };
 
 export const saveState = (state: State) => {
   try {
+    let userFeed = JSON.stringify(state.userFeed);
     let subscriptions = JSON.stringify(state.subscriptions);
+    localStorage.setItem('user-feed', userFeed);
     localStorage.setItem('subscriptions', subscriptions);
   } catch (err) {
     console.log(err);
