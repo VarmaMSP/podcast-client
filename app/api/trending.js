a2206453a0417ce27f2126a6bfa467588b280004
siva pavan <siva.pavan@juspay.in>
2018-05-30 15:01:07 +0530

    Fix formatting errors.

--- app/api/trending.js
+++ app/api/trending.js
@@ -4,7 +4,7 @@ import request from 'request'
 
 export default function getTrending (): Promise<Array<Podcast>> {
   let getUrl = window.location
-  let baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1]
+  let baseUrl = getUrl.protocol + '//' + getUrl.host + '/' + getUrl.pathname.split('/')[1]
   return new Promise((resolve, reject) => {
     request(baseUrl + '/json/trending.json', (err, response, body) => {
       if (err) return reject(err)
